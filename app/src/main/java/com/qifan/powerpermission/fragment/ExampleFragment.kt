932d2a9017cbc85ee0d565520a9fd95ddc5bb154
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-06-28 15:17:26 +0200

    add license check

--- app/src/main/java/com/qifan/powerpermission/fragment/ExampleFragment.kt
+++ app/src/main/java/com/qifan/powerpermission/fragment/ExampleFragment.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2020 by Qifan YANG (@underwindfall)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.qifan.powerpermission.fragment
 
 import android.Manifest
@@ -10,7 +25,12 @@ import androidx.fragment.app.Fragment
 import com.qifan.powerpermission.Permission
 import com.qifan.powerpermission.R
 import com.qifan.powerpermission.askPermissions
-import com.qifan.powerpermission.data.*
+import com.qifan.powerpermission.data.granted
+import com.qifan.powerpermission.data.hasAllGranted
+import com.qifan.powerpermission.data.hasPermanentDenied
+import com.qifan.powerpermission.data.hasRational
+import com.qifan.powerpermission.data.permanentDenied
+import com.qifan.powerpermission.data.rational
 import com.qifan.powerpermission.databinding.FragmentExampleBinding
 import com.qifan.powerpermission.rationale.createDialogRationale
 import com.qifan.powerpermission.rationale.delegate.RationaleDelegate
